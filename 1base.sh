5db4e47361afb81a4d6e9b054f08eda4189c141b
cytopyge <cytopyge@protonmail.com>
2019-05-14 06:22:40 +0200

    20190514_062239 (autopush)

--- 1base.sh
+++ 1base.sh
@@ -140,12 +140,12 @@ function set_boot_partition() {
 
 	read -p "the full BOOT partition is: '$boot_part', correct? (y/N) " -n 1 -r
 
-		if [[ $REPLY =~ ^[Yy]$ ]] ; then
-			printf "using '$boot_part' as BOOT partition\n"
-		else
-			echo
-			return 0 && set_boot_partition
-		fi
+	if [[ $REPLY =~ ^[Yy]$ ]] ; then
+		printf "using '$boot_part' as BOOT partition\n"
+	else
+		echo
+		return 0 && set_boot_partition
+	fi
 	echo
 
 }
@@ -159,12 +159,12 @@ function set_lvm_partition() {
 
 	read -p "the full LVM partition is: '$lvm_part', correct? (y/N) " -n 1 -r
 
-		if [[ $REPLY =~ ^[Yy]$ ]] ; then
-			printf "using '$lvm_part' as LVM partition\n"
-		else
-			echo
-			return 0 && set_lvm_partition
-		fi
+	if [[ $REPLY =~ ^[Yy]$ ]] ; then
+		printf "using '$lvm_part' as LVM partition\n"
+	else
+		echo
+		return 0 && set_lvm_partition
+	fi
 	echo
 
 }
