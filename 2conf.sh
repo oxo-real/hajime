f64514c43ac36399af4c6d955c6790a3487382bb
cytopyge <cytopyge@protonmail.com>
2019-12-07 08:51:09 +0100

    20191207_085051 (autopush)

--- 2conf.sh
+++ 2conf.sh
@@ -205,7 +205,8 @@ passwd $username
 ## priviledge escalation for wheel group
 sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /etc/sudoers
 
-
+## keep environment variable with elevated priviledges
+sed -i 's/# Defaults env_keep += "HOME"/Defaults env_keep += "HOME"\nDefaults !always_set_home, !set_home' /etc/sudoers
 
 
 # install helpers
