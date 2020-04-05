296eee03e346820b9524c6ae80f0f45756ece45a
user <user@protonmail.com>
2020-04-05 16:03:29 +0200

    20200405_160324 (autopush)

--- 0init.sh
+++ 0init.sh
@@ -1,28 +1,29 @@
 #!/bin/sh
 
-# just hit enter when eth
-read -p "wireless access point? " wap
 
-if [[ -z $wap ]]; then
-	connect
-else
-	setup_wap
-fi
+reply_single() {
+
+        # first entered character goes directly to $reply
+        stty_0=$(stty -g)
+	stty raw #-echo
+        reply=$(head -c 1)
+        stty $stty_0
+
+}
+
 
 setup_wap() {
 
-	echo "password? "
+	echo "enter password for $wap "
 	wpa_passphrase $wap > wap.wifi
-	wpa_supplicant -B -i wlan0 -c wap.wifi
-	connect
+	wpa_supplicant -B -i $interface -c wap.wifi
 
 }
 
 
 connect() {
 
-	dhcpcd
-	install
+	dhcpcd -w
 
 }
 
@@ -31,7 +32,48 @@ install() {
 
 	pacman -Sy --noconfirm git
 	git clone https://gitlab.com/cytopyge/hajime
+	echo
+	echo "sh hajime/1base.sh"
+	echo
 
 }
 
-echo "sh hajime/1base.sh"
+
+printf "hajime_0init\n"
+echo
+printf "connect to wireless access point? (y/N) "
+
+reply_single
+
+if printf "$reply" | grep -iq "^y" ; then
+	interface="wlan0"
+	setup_wap
+	connect
+	# not so neat, but ....
+	printf "connected succesfully"
+else
+	#interface="eth0"
+	connect
+	# not so neat, but ....
+	printf "connected succesfully"
+fi
+
+
+if [[ -f ~/hajime/1base.done ]]; then
+	# 1base.sh already ran
+	exit 10
+else
+	# 1base.sh has not yet ran
+	printf "install git & hajime? (Y/n) "
+
+	reply_single
+
+	if printf "$reply" | grep -iq "^n" ; then
+		exit 0
+	else
+		install
+	fi
+fi
+
+
+exit 0
